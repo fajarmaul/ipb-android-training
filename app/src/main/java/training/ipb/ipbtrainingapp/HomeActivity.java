285bedfc0e6108df64ed1e30b96e8ecb3c083aa5
Diman12345 <wardimanperdian@gmail.com>
2018-09-23 21:29:05 +0700

    Day 1 Project -2

--- app/src/main/java/training/ipb/ipbtrainingapp/HomeActivity.java
+++ app/src/main/java/training/ipb/ipbtrainingapp/HomeActivity.java
@@ -4,16 +4,63 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+//Version 1
+//public class HomeActivity extends AppCompatActivity {
+//    private Intent intent;
+//    private TextView namaTextView;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_home);
+//        intent = getIntent();
+//        namaTextView = findViewById(R.id.namaTextView);
+//        namaTextView.setText(intent.getStringExtra("data"));
+//    }
+//}
 
+////Version 2
+//public class HomeActivity extends AppCompatActivity {
+//    private Intent intent;
+//    private TextView namaTextView;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_home);
+//        intent = getIntent();
+//        namaTextView = findViewById(R.id.namaTextView);
+//        namaTextView.setText(intent.getStringExtra("data"));
+//    }
+//}
+
+//Version 3
 public class HomeActivity extends AppCompatActivity {
     private Intent intent;
+    private Bundle dataReceived;
+    private User userData;
     private TextView namaTextView;
+    private TextView namaTextViewDua;
+    private TextView umurTextView;
+    private TextView genderTextView;
+    private TextView alamatTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        intent = getIntent();
+        dataReceived = getIntent().getExtras();
+        userData = dataReceived.getParcelable("data");
+
+        //Binding the data
         namaTextView = findViewById(R.id.namaTextView);
-        namaTextView.setText(intent.getStringExtra("data"));
+        namaTextViewDua = findViewById(R.id.namaTextViewDua);
+        umurTextView = findViewById(R.id.umurTextView);
+        genderTextView = findViewById(R.id.genderTextView);
+        alamatTextView = findViewById(R.id.alamatTextView);
+
+        //Set the data to the view
+        namaTextView.setText(userData.getName());
+        namaTextViewDua.setText(userData.getName());
+        umurTextView.setText(String.valueOf(userData.getAge()));
+        genderTextView.setText(userData.getGender());
+        alamatTextView.setText(userData.getAddress());
     }
 }
--- app/src/main/java/training/ipb/ipbtrainingapp/MainActivity.java
+++ app/src/main/java/training/ipb/ipbtrainingapp/MainActivity.java
@@ -7,27 +7,102 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+//Version 1
+//public class MainActivity extends AppCompatActivity {
+//    private EditText namaEditText;
+//    private Button masukButton;
+//    private String nama;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        //Binding the view
+//        namaEditText = findViewById(R.id.inputNamaEditText);
+//        masukButton = findViewById(R.id.masukButton);
+//        //Click Listener
+//        masukButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                nama = namaEditText.getText().toString();
+//                //Creating Intent and put the data inside Intent
+//                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                intent.putExtra("data", nama);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//}
+
+//Version 2
+//public class MainActivity extends AppCompatActivity {
+//    private EditText namaEditText;
+//    private Button masukButton;
+//    private String nama;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        //Binding the view
+//        namaEditText = findViewById(R.id.inputNamaEditText);
+//        masukButton = findViewById(R.id.masukButton);
+//        //Click Listener
+//        masukButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (namaEditText.getText().toString().equals("")){
+//                    Toast.makeText(MainActivity.this, "Nama Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    nama = namaEditText.getText().toString();
+//                    //Creating Intent and put the data inside Intent
+//                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                    intent.putExtra("data", nama);
+//                    startActivity(intent);
+//                }
+//            }
+//        });
+//    }
+//}
+
+//Version 3 with POJO
 public class MainActivity extends AppCompatActivity {
-    private EditText namaEditText;
+    private EditText namaEditText, umurEditText, genderEditText, alamatEditText;
     private Button masukButton;
-    private String nama;
+    private String nama, gender, alamat;
+    private int umur;
+    private User userData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Binding the view
         namaEditText = findViewById(R.id.inputNamaEditText);
+        umurEditText = findViewById(R.id.inputUmurEditText);
+        genderEditText = findViewById(R.id.inputGenderEditText);
+        alamatEditText = findViewById(R.id.inputAlamatEditText);
         masukButton = findViewById(R.id.masukButton);
         //Click Listener
         masukButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nama = namaEditText.getText().toString();
-                //Creating Intent and put the data inside Intent
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                intent.putExtra("data", nama);
-                startActivity(intent);
+                if (namaEditText.getText().toString().equals("")||
+                        umurEditText.getText().toString().equals("")||
+                        genderEditText.getText().toString().equals("")||
+                        alamatEditText.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Data Masih Terdapat Yang Kosong", Toast.LENGTH_SHORT).show();
+                }else{
+                    nama = namaEditText.getText().toString();
+                    umur = Integer.valueOf(umurEditText.getText().toString());
+                    gender = genderEditText.getText().toString();
+                    alamat = alamatEditText.getText().toString();
+                    userData = new User(nama,umur,gender,alamat);
+
+                    //Creating Intent and put the data inside Intent
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    intent.putExtra("data", userData);
+                    startActivity(intent);
+                }
             }
         });
     }
